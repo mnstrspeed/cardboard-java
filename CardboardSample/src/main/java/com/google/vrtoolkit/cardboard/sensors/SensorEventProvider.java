eae6c571eaafbddbaa5a3077b9ec1a048f420692
Tom Sanders <github@tomsanders.nl>
2015-01-13 13:00:02 +0100

    Ran eclipse auto format on entire project

--- CardboardSample/src/main/java/com/google/vrtoolkit/cardboard/sensors/SensorEventProvider.java
+++ CardboardSample/src/main/java/com/google/vrtoolkit/cardboard/sensors/SensorEventProvider.java
@@ -2,13 +2,12 @@ package com.google.vrtoolkit.cardboard.sensors;
 
 import android.hardware.*;
 
-public interface SensorEventProvider
-{
-    void start();
-    
-    void stop();
-    
-    void registerListener(SensorEventListener p0);
-    
-    void unregisterListener(SensorEventListener p0);
+public interface SensorEventProvider {
+	void start();
+
+	void stop();
+
+	void registerListener(SensorEventListener p0);
+
+	void unregisterListener(SensorEventListener p0);
 }
--- CardboardSample/src/main/java/com/google/vrtoolkit/cardboard/sensors/SystemClock.java
+++ CardboardSample/src/main/java/com/google/vrtoolkit/cardboard/sensors/SystemClock.java
@@ -1,9 +1,8 @@
 package com.google.vrtoolkit.cardboard.sensors;
 
-public class SystemClock implements Clock
-{
-    @Override
-    public long nanoTime() {
-        return System.nanoTime();
-    }
+public class SystemClock implements Clock {
+	@Override
+	public long nanoTime() {
+		return System.nanoTime();
+	}
 }
